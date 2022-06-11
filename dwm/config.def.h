ef4c8d221023d803d5174d176bb5f9f620657bd9
Alok Shandilya <alok.shandilya10@gmail.com>
2022-06-11 14:07:01 +0530

    add cool autostart patch

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -61,6 +61,11 @@ static char *colors[][3] = {
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
+static const char *const autostart[] = {
+	"st", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
