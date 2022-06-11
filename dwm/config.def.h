9fcd3aa5414408c062807ca42f519e840c8099c0
Alok Shandilya <alok.shandilya10@gmail.com>
2022-06-11 21:59:45 +0530

    add awesome fonts to tags

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -83,7 +83,8 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+// static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
     /*  xprop(1):
