66e414598bb0709d0892e39e5f694548ed8057d4
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-09 10:32:46 +0530

    switch to dash in SHCMD(cmd)

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -169,7 +169,7 @@ static const Layout layouts[] = {
     {1, {{MODKEY|ControlMask|ShiftMask, KEY}},          toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/dash", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
