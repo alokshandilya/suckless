c2b91b2db759c448e67eb5f9a6596396f1bba722
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-10 14:43:37 +0530

    add desktop entry for dwm

--- install.sh
+++ install.sh
@@ -48,3 +48,37 @@ sudo systemctl enable lightdm
 
 # polkit
 paru -S gnome-keyring lxsession
+
+# lightdm entry
+printf "\n"
+printf "##########################################\n"
+printf "##### Creating desktop entry for dwm #####\n"
+printf "##########################################\n\n"
+cd
+DIR="/usr/share/xsessions"
+if [ -d "$DIR" ]; then
+    FILE=/usr/share/xsessions/dwm.desktop
+    if [ -f "$FILE" ]; then
+        printf "$FILE exists\n"
+        sudo rm -rf /usr/share/xsessions/dwm.desktop
+    fi
+    sudo touch /usr/share/xsessions/dwm.desktop
+    echo "[Desktop Entry]" | sudo tee -a /usr/share/xsessions/dwm.desktop
+    echo "Encoding=UTF-8" | sudo tee -a /usr/share/xsessions/dwm.desktop
+    echo "Name=dwm" | sudo tee -a /usr/share/xsessions/dwm.desktop
+    echo "Comment=Dynamic Window Manager" | sudo tee -a /usr/share/xsessions/dwm.desktop
+    echo "Exec=~/.dwm/autostart" | sudo tee -a /usr/share/xsessions/dwm.desktop
+else
+    sudo mkdir /usr/share/xsessions
+    sudo touch /usr/share/xsessions/dwm.desktop
+    echo "[Desktop Entry]" | sudo tee -a /usr/share/xsessions/dwm.desktop
+    echo "Encoding=UTF-8" | sudo tee -a /usr/share/xsessions/dwm.desktop
+    echo "Name=dwm" | sudo tee -a /usr/share/xsessions/dwm.desktop
+    echo "Comment=Dynamic Window Manager" | sudo tee -a /usr/share/xsessions/dwm.desktop
+    echo "Exec=~/.dwm/autostart" | sudo tee -a /usr/share/xsessions/dwm.desktop
+fi
+
+printf "\n"
+printf "##########################################\n"
+printf "###### dwm is successfully installed######\n"
+printf "##########################################\n\n"
