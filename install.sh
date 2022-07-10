6aa164be7fa3dc992c407bc0915224e0bba5ca82
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-10 05:42:29 +0530

    check paru and install if not installed

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+########################################
+##### Installing paru (AUR Helper) #####
+########################################
+if ! command -v paru &> /dev/null
+then
+    printf "\n"
+    printf "########################################\n"
+    printf "##### Installing paru (AUR Helper) #####\n"
+    printf "########################################\n\n"
+    git clone https://aur.archlinux.org/paru-bin.git
+    cd paru-bin
+    makepkg -si
+else
+    printf "\n"
+    printf "#######################################\n"
+    printf "###### paru is already installed ######\n"
+    printf "#######################################\n"
+fi
+
+printf "\n"
 printf "#########################################\n"
 printf "##### Installing suckless utilities #####\n"
 printf "#########################################\n\n"
