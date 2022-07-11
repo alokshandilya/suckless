ff4b278159e8b9e4ac4a3a2c9930588d33f3a76c
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-11 07:28:50 +0530

    change memory nerd font icon

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -6,10 +6,10 @@ static const Block blocks[] = {
     {"^C5^ ﮮ ^d^", "$HOME/.config/scripts/upt",         60,                     2},
     {"^C3^  ^d^", "$HOME/.config/scripts/pacupdate",   360,                    5},
     {"^C4^  ^d^", "$HOME/.config/scripts/weather",     60,                     1},
-    // {"^C1^   ^d^", "$HOME/.config/scripts/kernel",      360,                    2},
+    // {"^C1^  ^d^", "$HOME/.config/scripts/kernel",      360,                    2},
     {"^C6^  ^d^", "$HOME/.config/scripts/cpu",         2,                      1},
-    {"^C5^ 瑱 ^d^", "$HOME/.config/scripts/memory",      6,                      1},
-    {"^C3^   ^d^", "$HOME/.config/scripts/battery",     150,                    1},
+    {"^C5^ 瑱 ^d^", "$HOME/.config/scripts/memory",     6,                      1},
+    {"^C3^   ^d^", "$HOME/.config/scripts/battery",    150,                    1},
     {"^C4^  ^d^", "$HOME/.config/scripts/volume",      2,                      5},
     {"^C1^  ^d^", "$HOME/.config/scripts/clock",       5,                      0},
 
