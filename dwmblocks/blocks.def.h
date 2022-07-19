2656a40694f63e55a1d292312c47f6043687f15b
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-20 01:44:48 +0530

    change dwmblocks scripts path
    - change path from ~/.config/scripts to ~/.local/bin/dwmblocks
    - remove nerd fonts entry

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,17 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/       /*Command*/                         /*Update Interval*/     /*Update Signal*/
-
-    // AWESOME FONTS :
-    {"^C5^ ﮮ ^d^", "$HOME/.config/scripts/upt",         60,                     2},
-    {"^C3^  ^d^", "$HOME/.config/scripts/pacupdate",   360,                    5},
-    {"^C4^  ^d^", "$HOME/.config/scripts/weather",     60,                     1},
-    // {"^C1^  ^d^", "$HOME/.config/scripts/kernel",      360,                    2},
-    {"^C6^  ^d^", "$HOME/.config/scripts/cpu",         2,                      1},
-    {"^C5^ 瑱 ^d^", "$HOME/.config/scripts/memory",     6,                      1},
-    {"^C3^   ^d^", "$HOME/.config/scripts/battery",    150,                    1},
-    {"^C4^  ^d^", "$HOME/.config/scripts/volume",      2,                      5},
-    {"^C1^  ^d^", "$HOME/.config/scripts/clock",       5,                      0},
+  //Icon               Command                                  Update Interval         Update Signal  
+  // AWESOME FONTS :
+  {"^C5^ ﮮ ^d^",      "$HOME/.local/bin/dwmblocks/upt",         60,                     2},
+  {"^C3^  ^d^",      "$HOME/.local/bin/dwmblocks/pacupdate",   360,                    5},
+  {"^C4^  ^d^",      "$HOME/.local/bin/dwmblocks/weather",     60,                     1},
+  // {"^C1^  ^d^",      "$HOME/.local/bin/dwmblocks/kernel",      360,                    2},
+  {"^C6^  ^d^",      "$HOME/.local/bin/dwmblocks/cpu",         2,                      1},
+  {"^C5^ 瑱 ^d^",     "$HOME/.local/bin/dwmblocks/memory",      6,                      1},
+  {"^C3^   ^d^",     "$HOME/.local/bin/dwmblocks/battery",     150,                    1},
+  {"^C4^  ^d^",      "$HOME/.local/bin/dwmblocks/volume",      2,                      5},
+  {"^C1^  ^d^",      "$HOME/.local/bin/dwmblocks/clock",       5,                      0},
 
     // ICONS [requires libxft-bgra]
     // {" 🔺 ",        "$HOME/.config/scripts/upt",         60,                     2},
@@ -23,18 +22,6 @@ static const Block blocks[] = {
     // {" 🔋 ",        "$HOME/.config/scripts/battery",     150,                    1},
     // {" 🔊 ",        "$HOME/.config/scripts/volume",      2,                      5},
     // {" 🕑 ",        "$HOME/.config/scripts/clock",       5,                      0},
-
-    // NERD FONTS :
-    // {"^C5^ 卑 ^d^", "$HOME/.config/scripts/upt",         60,                     2},
-    // {"^C3^   ^d^", "$HOME/.config/scripts/pacupdate",   360,                    9},
-    // {"^C4^   ^d^", "$HOME/.config/scripts/weather",     60,                     1},
-    // {"^C1^   ^d^", "$HOME/.config/scripts/kernel",      360,                    2},
-    // {"^C6^   ^d^", "$HOME/.config/scripts/cpu",         2,                      1},
-    // {"^C5^   ^d^", "$HOME/.config/scripts/memory",      6,                      1},
-    // {"^C3^   ^d^", "$HOME/.config/scripts/battery",     150,                    1},
-    // {"^C4^   ^d^", "$HOME/.config/scripts/volume",      2,                      5},
-    // {"^C1^   ^d^", "$HOME/.config/scripts/clock",       5,                      0},
-
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
