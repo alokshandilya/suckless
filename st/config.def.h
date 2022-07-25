d69a837d63df99295c2448a66dcf3f6d580f0f2a
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-25 09:51:53 +0530

    add glyph wide support patch

--- st/config.def.h
+++ st/config.def.h
@@ -5,11 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=15:style:extrabold:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"JetBrainsMono Nerd Font:pixelsize=15:style:extrabold:antialias=true:autohint=true",
+	"Twemoji:pixelsize=15:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
