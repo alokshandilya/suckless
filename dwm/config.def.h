771db5a1756aa78f6a05ac43838d29658ca770ac
Alok Shandilya <alok.shandilya10@gmail.com>
2022-09-08 20:43:48 +0530

    increase borderpx to 3

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -10,7 +10,7 @@
 #define FILEMANAGER "pcmanfm"
 
 // appearance
-static const unsigned int borderpx  = 2;        // border pixel of windows
+static const unsigned int borderpx  = 3;        // border pixel of windows
 static const unsigned int snap      = 32;       // snap pixel
 static const unsigned int gappih    = 10;       // horiz inner gap between windows
 static const unsigned int gappiv    = 10;       // vert inner gap between windows
