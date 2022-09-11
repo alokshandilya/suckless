43e55bc29d66d47014bed279fc7461e0de54d1f2
Alok Shandilya <alok.shandilya10@gmail.com>
2022-09-11 17:01:20 +0530

    gruvbox material colorscheme

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#d4be98", "#1d2021" },
+	[SchemeSel] = { "#d4be98", "#89b482" },
+	[SchemeOut] = { "#d4be98", "#a9b665" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
